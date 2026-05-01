5a4d1e2e37f0a5ce1707e69904a361bdaee954d2
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-05-01 23:04:43 +0600

    mongodb client promise fixed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,11 +2,23 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("suncart");
+const uri = process.env.MONGODB_URI;
+
+if (!global._mongoClientPromise) {
+    const client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+}
+
+const clientPromise = global._mongoClientPromise;
+const connectedClient = await clientPromise;
+const db = connectedClient.db("suncart");
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db, { client }),
+    database: mongodbAdapter(db, { client: connectedClient }),
+    trustedOrigins: [
+        "https://ph-assignment-08-suncart-83dg.vercel.app",
+        "http://localhost:3000",
+    ],
     emailAndPassword: {
         enabled: true,
     },
