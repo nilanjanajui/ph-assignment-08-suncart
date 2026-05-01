1a815cc5712c138304354590e42f5fe43a961fa1
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-05-01 23:13:32 +0600

    force to deploy

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,19 +2,15 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const uri = process.env.MONGODB_URI;
-
-if (!global._mongoClientPromise) {
-    const client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+function getDb() {
+    if (!global._mongoClient) {
+        global._mongoClient = new MongoClient(process.env.MONGODB_URI);
+    }
+    return global._mongoClient;
 }
 
-const clientPromise = global._mongoClientPromise;
-const connectedClient = await clientPromise;
-const db = connectedClient.db("suncart");
-
 export const auth = betterAuth({
-    database: mongodbAdapter(db, { client: connectedClient }),
+    database: mongodbAdapter(getDb().db("suncart"), { client: getDb() }),
     trustedOrigins: [
         "https://ph-assignment-08-suncart-83dg.vercel.app",
         "http://localhost:3000",
